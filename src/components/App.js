e5eab0a43aa4a3729b1901c158b1694ac568e202
AlexanderGninenko <alexander.gninenko@icloud.com>
2022-07-30 11:43:52 +1000

    ADDED path

--- src/components/App.js
+++ src/components/App.js
@@ -231,7 +231,7 @@ function App() {
           <Header loggedIn={loggedIn} email={email} onSignOut={handleSignOut} />
           <ProtectedRoute
             exact
-            path="/react-mesto-auth"
+            path="/react-mesto-auth/"
             component={Main}
             loggedIn={loggedIn}
             cards={cards}
@@ -242,13 +242,13 @@ function App() {
             onCardClick={handleCardClick}
             onConfirmCardDelete={handleConfirmDeleteClick}
           />
-          <Route path="/react-mesto-auth/sign-up">
+          <Route path="/react-mesto-auth/sign-up/">
             <Register
               onRegister={handleRegistration}
               isLoading={isRenderLoading}
             />
           </Route>
-          <Route path="/react-mesto-auth/sign-in">
+          <Route path="/react-mesto-auth/sign-in/">
             <Login
               onLogin={handleAuthorization}
               isLoading={isRenderLoading}
